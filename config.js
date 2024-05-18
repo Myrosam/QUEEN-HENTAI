29b70fe4128bc1bb75c1796fd55f96b13f35eb72
Myrosam <151370829+Myrosam@users.noreply.github.com>
2024-05-18 23:30:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
+  ['60104440769', 'Adam Official²⁰²³', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
